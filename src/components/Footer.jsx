0acebdd31fb79cfe4ccec9f0545f7c161489598d
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-05-18 16:35:30 +0100

    footer-lower

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,6 +17,36 @@ let Footer = () => {
                         <button>subscribe</button>
                 </form>
             </div>
+            <div className='mid-line'></div>
+            <div className='lower'>
+                <div className='footer-about'>
+                <div>
+            <a href="#"><img width={180} height={70} id="logo" src="https://demo.phlox.pro/bike-shop/wp-content/uploads/sites/107/2019/05/cropped-logo.png" alt="logo"/></a>                
+            </div>
+            <p >we ship every where! free in the us</p>
+            <p>© 2024 Phlox Shop - Phlox Elementor WordPress Theme. All rights reserved.</p>
+                </div>
+                <div className='footer-contact'>
+                <h3 className='footer-h3-margin'>Contact :</h3>
+                    <p className='footer-p'>Info@yourdomain.com</p>
+                    <p className='footer-p'>+1 403-253-7717</p>
+                </div>
+                <div className='footer-address'>
+                <h3 className='footer-h3-margin'>Address :</h3>
+                    <p className='footer-p'>123 Ave, Lorem City, Site Country, the World</p>
+                </div>
+                <div className='footer-social'>
+                    <h3 className='footer-h3-margin'>Social :</h3>
+                    <div>
+                        <a className='footer-p' href="#">Instagram</a>
+                        <a className='footer-p' href="#">Twitter</a>
+                        <a className='footer-p' href="#">Facebook</a>
+                        <a className='footer-p' href="#">LinkedIn</a>
+                        <a className='footer-p' href="#">Youtube </a>
+                        <a className='footer-p' href="#">Vimeo</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
