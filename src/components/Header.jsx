f029f70829bb86685541a9fce0feb7bbc1b41032
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-05-18 22:15:26 +0100

    header and footer responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faBagShopping, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 let Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    let handleClick = () =>{
+        setIsOpen(!isOpen);
+    }
     return(
         <nav className="header">
+        <div className="smaller-screen" style={{display: isOpen ? "flex" :  "none"}}>
+        <FontAwesomeIcon style={{fontSize: 50, cursor: "pointer"}} onClick={handleClick} icon={faCircleXmark} />
+        <ul>
+                
+                <li>Home</li>
+                    <li>Bike</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+        </div>
+            <div className="big-screen">
             <div className="header-right">
             <div>
             <a href="#"><img width={180} height={70} id="logo" src="https://demo.phlox.pro/bike-shop/wp-content/uploads/sites/107/2019/05/cropped-logo.png" alt="logo"/></a>                
@@ -12,16 +27,26 @@ let Header = () => {
             <div className="middle-line"></div>
                 <FontAwesomeIcon icon={faMagnifyingGlass} className="search-logo"/>
             </div>
-            <ul className="header-links">
+           
+            <div  className="header-links">
+            <div className="menu-button" onClick={handleClick}>
+          &#9776;
+        </div>
+            <ul>
                     <li>Home</li>
                     <li>Bike</li>
                     <li>Blog</li>
                     <li>Contact</li>
-                    <li>
-                    <span>0</span>
-                    <FontAwesomeIcon icon={faBagShopping} />
-                    </li>
                 </ul>
+                <div className="cart">
+                <span>0</span>
+                    <FontAwesomeIcon icon={faBagShopping} />
+                </div>
+                
+            </div>
+            </div>
+          
+            
         </nav>
     )
 }
